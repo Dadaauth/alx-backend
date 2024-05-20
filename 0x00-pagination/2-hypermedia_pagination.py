88d8e3947da27ab816d422af1035052071c0cf2e
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-05-20 16:34:29 +0100

    ALX: Project: Pagination: Task #2: Second Attempt: Used the math.ceil() function to round up floats

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -54,6 +54,6 @@ class Server:
             "data": data,
             "next_page": page + 1 if page < (len(dataset) / page_size) else None,
             "prev_page": page - 1 if page > 1 else None,
-            "total_pages": round(len(dataset) / page_size)
+            "total_pages": math.ceil(len(dataset) / page_size)
         }
         return rtn_dict
