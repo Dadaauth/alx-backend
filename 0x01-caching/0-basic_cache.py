e6a983532733bf6ee3d12294e6887266a5e5b37f
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-05-22 14:56:34 +0100

    ALX: Project: Caching: Task #0: Second attempt: Wrote documentation for my classes and methods

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,13 +4,16 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """A class documentation that I wrote"""
 
     def put(self, key, item):
+        """A function documentation that I wrote"""
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """A function documentation that I wrote"""
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
